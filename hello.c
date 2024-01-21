0e886ea9af269d4688994ab61156bf693214abf8
pedro <pdro181205@gmail.com>
2024-01-21 20:00:20 -0300

    -a

---
+++ hello.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+
+int main() {
+    // Imprime a mensagem "Hello, World!" na tela
+    printf("Hello, World!\n");
+
+    // Retorna 0 para indicar que o programa foi executado com sucesso
+    return 0;
+}
---
+++ hello.py
@@ -0,0 +1,4 @@
+# inicio
+def hello(name='world'):
+    print (f"Hello, {name}")
+hello("my friend oooo")
